e2f773c071812536469df715e2b008f3eee7bbd0
Ragesh-iTech <ragesh12082005@gmail.com>
2026-01-21 23:02:53 +0530

    first commit

--- js/emailjs.js
+++ js/emailjs.js
@@ -1,24 +1,24 @@
 
-function sendemail(){
+function sendemail() {
 
-	var email=document.getElementById("email").value;
-	var to_name=document.getElementById("name").value;
-    var subject =document.getElementById("subject").value;
-	var message=document.getElementById("message").value;
+    var email = document.getElementById("email").value;
+    var to_name = document.getElementById("name").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
 
-	var templateParams = {
+    var templateParams = {
         email: email,
         name: to_name,
-        subject:subject,
-		message: message
-      };
+        subject: subject,
+        message: message
+    };
 
-      emailjs.send('service_zw2t1n8', 'template_m77xzzf', templateParams)
-  .then(function(response) {
-     console.log('SUCCESS!', response.status, response.text);
-     window.alert("Sent successfully!");
-     
-  })
+    emailjs.sendForm('service_i66orw9', 'template_rjqxkkc', templateParams)
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+            window.alert("Sent successfully!");
 
-     
+        })
+
+
 }
